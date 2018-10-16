80e79cb98edaeb109952a349226e6c187038168e
Chace Powell <cpcpowell8@gmail.com>
2018-10-16 16:56:15 -0400

    change text param

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,6 @@
-var db = require("../models");
+//foodtrucksniffer.herokuapp.com/var db = require("../models");
 
-module.exports = function(app) {
+https: module.exports = function(app) {
   // Load index page
   // app.get("/", function(req, res) {
   //   res.sendFile(path.join(__dirname, "../public/index.html"));
