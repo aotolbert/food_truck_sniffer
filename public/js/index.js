d85d4ac10ef104d0703aff20d4db0de302b887ea
Chace Powell <cpcpowell8@gmail.com>
2018-10-25 19:39:24 -0400

    remove index.handlebars as it was functionally useless. move all the js in index.html into a seperate file named index.js. rename the random character js files that made mapData and mapStyling to mapData.js and mapStyling.js. format index.html.

--- public/js/index.js
+++ public/js/index.js
@@ -1,99 +1,182 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
+var truckOBJs = [];
+let servicesOBJ = {};
+let dataPointsLocationOBJ = {};
+let dataPointsServicesOBJ = {};
+let servicesID;
 
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  }
-};
+// \/\/\/ Used for labeling the markers on map \/\/\/ 
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789';
+var labelIndex = 0;
 
-// refreshExamples gets new examples from the db and repopulates the list
-var refreshExamples = function() {
-  API.getExamples().then(function(data) {
-    var $examples = data.map(function(example) {
-      var $a = $("<a>")
-        .text(example.text)
-        .attr("href", "/example/" + example.id);
+var gmarkers = [];
+var infowindow = new google.maps.InfoWindow();
 
-      var $li = $("<li>")
-        .attr({
-          class: "list-group-item",
-          "data-id": example.id
-        })
-        .append($a);
+var locationSource = $("#location-template").html();
+var locTemplate = Handlebars.compile(locationSource);
+console.log("Location Source: ", locationSource)
 
-      var $button = $("<button>")
-        .addClass("btn btn-danger float-right delete")
-        .text("ｘ");
+var serviceSource = $("#services-template").html();
+var serviceTemplate = Handlebars.compile(serviceSource);
 
-      $li.append($button);
-
-      return $li;
-    });
-
-    $exampleList.empty();
-    $exampleList.append($examples);
-  });
-};
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-
-  var example = {
-    text: $exampleText.val().trim(),
-    description: $exampleDescription.val().trim()
+function initialize() {
+  var center = new google.maps.LatLng(33.4483771, -112.07403729999999);
+  var mapOptions = {
+    zoom: 15,
+    center: { lat: 35.22, lng: -80.84 },
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: mapStyle
   };
 
-  if (!(example.text && example.description)) {
-    alert("You must enter an example text and description!");
-    return;
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  var geocoder = new google.maps.Geocoder();
+  //populates truckOBJs array with addresses from the database.
+  $.ajax("/api/trucks/", {
+    type: "GET",
+  }).then(
+    function (response) {
+      for (let i = 0; i < response.length; i++) {
+        let newOBJ = response[i];
+        console.log("Object being added to truckOBJs: ", newOBJ);
+        truckOBJs.push(newOBJ);
+      }
+      geocodeAddress(geocoder, map);
+    }
+  );
+
+}
+function geocodeAddress(geocoder, resultsMap) {
+  for (let i = 0; i < truckOBJs.length; i++) {
+    geocoder.geocode({ 'address': truckOBJs[i].address }, function (results, status) {
+      resultsMap.setZoom(13);
+      longitude = results[0].geometry.viewport.getCenter().lng();
+      //console.log(longitude);
+
+      latitude = results[0].geometry.viewport.getCenter().lat();
+      var latLong = "" + latitude + " , " + longitude + "";
+      servicesID = truckOBJs[i].id;
+      servicesOBJ = truckOBJs[i];
+
+      locationData = { name: truckOBJs[i].name }
+      dataPointsLocationOBJ = {
+        ID: servicesID,
+        type: "Main",
+        latLong: latLong,
+        truckOBJ: truckOBJs[i],
+        taxonomy: {
+          locations: [
+            '123-432-234',
+            '4324-234324-2432'
+          ],
+          services: [
+            servicesID,
+            // '222-222-222'
+          ]
+        }
+      };
+      dataPointsServicesOBJ = {
+        latLong: latLong,
+        taxonomy: truckOBJs[i].id
+      };
+
+      mapData.key.serviceData.services[servicesID] = servicesOBJ;
+      mapData.key.locationData.locations[servicesID] = locationData;
+      mapData.dataPoints.services[servicesID] = dataPointsServicesOBJ;
+      mapData.dataPoints.locations.push(dataPointsLocationOBJ);
+      console.log("MAP DATA OBJ: ", mapData);
+
+
+
+
+      if (status === 'OK') {
+        resultsMap.setCenter(results[0].geometry.location);
+      }
+      // var marker = new google.maps.Marker({
+      // map: resultsMap,
+      //  zoom: 10,
+      // position: results[0].geometry.location,
+      //  label: labels[labelIndex++ % labels.length],
+      //  animation: google.maps.Animation.DROP,
+      //  customInfo: "Marker " + v,
+
+      // });
+      for (i = 0; i < mapData.dataPoints.locations.length; i++) {
+        addMarker(mapData.dataPoints.locations[i]);
+      };
+    })
   }
-
-  API.saveExample(example).then(function() {
-    refreshExamples();
-  });
-
-  $exampleText.val("");
-  $exampleDescription.val("");
 };
 
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
+function addMarker(marker) {
+  var location = getLocation(marker);
+  if (location) {
+    var category = marker.type
+    var title = location.name;
+    var latLong = marker.latLong.split(',');
+    var pos = new google.maps.LatLng(latLong[0], latLong[1]);
+    var content = getLocationContent(location);
 
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
+    $.extend(location, marker)
+
+    mapMarker = new google.maps.Marker({
+      title: title,
+      position: pos,
+      category: category,
+      locationData: location,
+      map: map
+    });
+
+    gmarkers.push(mapMarker);
+
+    // Marker click listener
+    google.maps.event.addListener(mapMarker, 'click', (function (mapMarker, content) {
+      return function () {
+        console.log("You clicked me: ", mapMarker.locationData.truckOBJ.twitterId, "=================");
+        // infowindow.setContent(content);
+        // infowindow.open(map, mapMarker);
+        //map.panTo(this.getPosition());
+        generateRelated(mapMarker);
+        $('#scrolly').removeAttr('style');
+
+      }
+    })(mapMarker, content));
+  }
+}
+
+function getLocation(marker) {
+  var location = false;
+  if (mapData.key.locationData.locations[marker.ID]) {
+    location = mapData.key.locationData.locations[marker.ID];
+  }
+  return location;
+}
+
+function getLocationContent(location) {
+  console.log("get location content: ", location)
+  return locTemplate(location);
+}
+
+function generateRelated(marker) {
+  $('#output').text('');
+  $('#output').html(generateServices(marker));
+}
+
+function generateServices(location) {
+  console.log("generateServices: ", location)
+  var serviceCards = [];
+
+  for (var x in location.locationData.taxonomy.services) {
+    var serviceID = location.locationData.taxonomy.services[x];
+    serviceCards.push(serviceTemplate(mapData.key.serviceData.services[serviceID]));
+    console.log("sent this to template", mapData.key.serviceData.services[serviceID]);
+
+  }
+  return serviceCards.join('')
+}
+
+function twitterEmbed(truckTwitterName) {
+  var tweetDiv = $("#twitterDisplay");
+  var embedElement = `<a class="twitter-timeline" data-tweet-limit="2" data-height="300" href="https://twitter.com/${truckTwitterName}"></a>`
+  $("#twitterDisplay").append(embedElement);
 };
 
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-$exampleList.on("click", ".delete", handleDeleteBtnClick);
+initialize();
