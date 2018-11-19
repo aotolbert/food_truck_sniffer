c35830c79dd26d4cee8a6913ad7906d2306e056a
Blake Robert <Blaker.blaker4@gmail.com>
2018-11-19 07:09:27 -0500

    googleapis now succesfully updates the fusion tables api

--- helper/fusionHelper.js
+++ helper/fusionHelper.js
@@ -1,9 +1,9 @@
-var fetch = require("fetch").fetchUrl;
+var googleapis = require("./googleapisHelper");
 var db = require("../models");
 let truckOBJs = []
 let sqlStatments = ""
 
-// setTimeout(prepareGoogleinsertCall, 30 * 1000)
+setTimeout(prepareGoogleinsertCall, 5 * 1000)
 
 function prepareGoogleinsertCall() {
     console.log("prepareGoogleinsert ran")
@@ -22,20 +22,11 @@ function prepareGoogleinsertCall() {
             let image = truckOBJs[a].image;
             let priceRating = truckOBJs[a].priceRating;
             let YelpReviews = truckOBJs[a].YelpReviews;
-            sqlStatments = sqlStatments + `INSERT INTO 1vJIoCXgQIU8nFSnQ3pfyw_kWhgnIdh8GmCRe-2If (TwitterId, Name, Phone, url, Address, overallRating, image, priceRating, YelpReviews) VALUES ("${TwitterId}","${Name}", "${Phone}", "${url}", "${Address}", "${overallRating}", "${image}", "${priceRating}", "${YelpReviews}");`
+            sqlStatments = sqlStatments + `INSERT INTO 1vJIoCXgQIU8nFSnQ3pfyw_kWhgnIdh8GmCRe-2If (TwitterId, Name, Phone, url, Address, overallRating, image, priceRating, YelpReviews) VALUES ('${TwitterId}','${Name}', '${Phone}', '${url}', '${Address}', '${overallRating}', '${image}', '${priceRating}', '${YelpReviews}');`
         }
     }).then(function () {
         console.log("This is the sqlStatments: ", sqlStatments)
-        googleApiCall("POST", sqlStatments).then(console.log);
+        googleapis(sqlStatments)
     })
 }
 
-function googleApiCall(method, body) {
-    return new Promise((resolve, reject) => {
-        fetch(`https://www.googleapis.com/fusiontables/v2/querysql=${body}&key=AIzaSyCqVSdnRqX7ARJawUR_LXPnuV7-a9Yk-u0`, {
-            method: method,
-        }, function (error, meta, body) {
-            console.log(error, meta, body)
-        })
-    })
-};
--- helper/googleapisHelper.js
+++ helper/googleapisHelper.js
@@ -1,43 +1,24 @@
 'use strict';
 
 const {google} = require('googleapis');
-const path = require('path');
+// const path = require('path');
+// var google = require('googleapis');
+var fusiontables = google.fusiontables('v2');
 
-/**
- * The JWT authorization is ideal for performing server-to-server
- * communication without asking for user consent.
- *
- * Suggested reading for Admin SDK users using service accounts:
- * https://developers.google.com/admin-sdk/directory/v1/guides/delegation
- *
- * See the defaultauth.js sample for an alternate way of fetching compute credentials.
- */
-async function googleSQLquery(query) {
-  console.log("Google Api call ran!!!!!")
-  // Create a new JWT client using the key file downloaded from the Google Developer Console
-  const client = await google.auth.getClient({
-    keyFile: path.join(__dirname, 'jwt.keys.json'),
-    scopes: ['https://www.googleapis.com/auth/fusiontables','https://www.googleapis.com/auth/fusiontables.readonly'],
-  });
+function googleSQLquery(sqlStatement) {
+var jwtClient = new google.auth.JWT(
+  process.env.GOOGLEAPIS_EMAIL, // I store my stuff in an .env file so it's not in the file
+  './MYNEWFILE.pem', //wherever you can access this file
+  null,
+  ['https://www.googleapis.com/auth/fusiontables', 'https://www.googleapis.com/auth/fusiontables.readonly'] //scope
+);
+jwtClient.authorize(function (err, tokens) {
+  if (err) {
+    console.log(err);
+    return;
+  } else {
+    const res = fusiontables.query.sql({auth: jwtClient, sql: sqlStatement });
+  }
+})}
 
-  // Obtain a new fusiontables client, making sure you pass along the auth client
-  const fusiontables = google.fusiontables({
-    version: 'v2',
-    auth: client,
-  });
-
-  // Make an authorized request to Fusion Tables API.
-  const res = await fusiontables.query.sql({sql:query});
-  console.log(res.data);
-
-  return res.data;
-}
-setTimeout(delay, 30 * 1000);
-function delay(){
-  googleSQLquery("INSERT INTO 1vJIoCXgQIU8nFSnQ3pfyw_kWhgnIdh8GmCRe-2If (TwitterId, Name, Phone, url, Address, overallRating, image, priceRating, YelpReviews) VALUES ('@HiyaFoodTruck','Hiya Food Truck', '(240) 670-4492', 'http://www.hiyafoodtruck.com/', '620 W Morehead St', '4', 'https://s3-media1.fl.yelpcdn.com/bphoto/CdXnN6QmOSTfaw0UsQZ04w/o.jpg', '$', 'undefined')")
-  .catch(console.error);
-  
-}
-
-
-module.exports = {googleSQLquery};
+module.exports = googleSQLquery;
