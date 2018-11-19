d7d4f396f4b07654d9c9b55a0f39d2bc409b9c27
Blake Robert <Blaker.blaker4@gmail.com>
2018-11-19 05:45:13 -0500

    testing google apis on test server

--- server.js
+++ server.js
@@ -15,49 +15,49 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 //Config for Twitter Webhook
-const webhook = twitterWebhook.userActivity({
-  serverUrl: "https://foodtrucksniffer.herokuapp.com",
-  route: "/webhook/twitter",
-  consumerKey: process.env.TWITTER_API_KEY,
-  consumerSecret: process.env.TWITTER_API_SECRET,
-  accessToken: process.env.TWITTER_ACCESS_TOKEN,
-  accessTokenSecret: process.env.TWITTER_ACCESS_SECRET,
-  environment: process.env.TWITTER_WEBHOOK_ENV
-});
+// const webhook = twitterWebhook.userActivity({
+//   serverUrl: "https://foodtrucksniffer.herokuapp.com",
+//   route: "/webhook/twitter",
+//   consumerKey: process.env.TWITTER_API_KEY,
+//   consumerSecret: process.env.TWITTER_API_SECRET,
+//   accessToken: process.env.TWITTER_ACCESS_TOKEN,
+//   accessTokenSecret: process.env.TWITTER_ACCESS_SECRET,
+//   environment: process.env.TWITTER_WEBHOOK_ENV
+// });
 
-//Checks for registered webhook. Registers & subscribes if none is found.
-webhook.getWebhook().then(data => {
-  if (!data[0].valid) {
-    webhook.register();
+// //Checks for registered webhook. Registers & subscribes if none is found.
+// webhook.getWebhook().then(data => {
+//   if (!data[0].valid) {
+//     webhook.register();
 
-    webhook.subscribe({
-      userId: process.env.TWITTER_USER_ID,
-      accessToken: process.env.TWITTER_ACCESS_TOKEN,
-      accessTokenSecret: process.env.TWITTER_ACCESS_SECRET
-    });
-  }
-});
+//     webhook.subscribe({
+//       userId: process.env.TWITTER_USER_ID,
+//       accessToken: process.env.TWITTER_ACCESS_TOKEN,
+//       accessTokenSecret: process.env.TWITTER_ACCESS_SECRET
+//     });
+//   }
+// });
 
-//On Twitter event, update the database with new address.
-webhook.on("event", (event, userId, data) => {
-  const arr = data.text.split("|")[0];
-  const address = arr
-    .split(" ")
-    .slice(1)
-    .join(" ");
+// //On Twitter event, update the database with new address.
+// webhook.on("event", (event, userId, data) => {
+//   const arr = data.text.split("|")[0];
+//   const address = arr
+//     .split(" ")
+//     .slice(1)
+//     .join(" ");
 
-  db.FoodTruck.update(
-    {
-      address: address,
-      addressUpdated: data.created_at
-    },
-    {
-      where: {
-        twitterId: `@${data.user.screen_name}`
-      }
-    }
-  ).then(udpatedLocation => console.log(udpatedLocation));
-});
+//   db.FoodTruck.update(
+//     {
+//       address: address,
+//       addressUpdated: data.created_at
+//     },
+//     {
+//       where: {
+//         twitterId: `@${data.user.screen_name}`
+//       }
+//     }
+//   ).then(udpatedLocation => console.log(udpatedLocation));
+// });
 
 // Handlebars
 app.engine(
